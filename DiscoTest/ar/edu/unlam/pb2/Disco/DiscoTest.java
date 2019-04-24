28878891bc66f22973349070e4d3a4bacb39cad4
perlamoly <pengate@gmail.com>
2019-04-24 00:12:56 -0300

    prueba commit

---
+++ DiscoTest/ar/edu/unlam/pb2/Disco/DiscoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.Disco;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiscoTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
---
+++ src/ar/edu/unlam/pb2/Disco/Disco.java
@@ -0,0 +1,47 @@
+package ar.edu.unlam.pb2.Disco;
+
+public class Disco {
+	private Double radioInterior;
+	private Double radioExterior;
+
+	public Double getRadioInterior() {
+		return radioInterior;
+	}
+
+	public void setRadioInterior(Double radioInterior) {
+		this.radioInterior = radioInterior;
+	}
+
+	public Double getRadioExterior() {
+		return radioExterior;
+	}
+
+	public void setRadioExterior(Double radioExterior) {
+		this.radioExterior = radioExterior;
+	}
+
+	public Disco() {
+
+	}
+
+	public void calcularPerimetroInterior(Double radioInterior) {
+		Double perimetroInterior = (2 * 3.14 * this.radioInterior);
+
+	}
+
+	public void calcularPerimetroExterior(Double radioExterior) {
+		Double perimetroExterior = (2 * 3.14 * this.radioExterior);
+	}
+
+	public Double calcularSuperficie(Double radioInterior, Double radioExterior) {
+		Double superficieTotal = 0.0;
+		Double superficie1 = (3.14 * this.radioInterior * this.radioInterior);
+		Double superficie2 = (3.14 * this.radioExterior * this.radioExterior);
+		if (radioInterior < radioExterior && radioInterior > 0)
+			superficieTotal = (superficie2 - superficie1);
+		return superficieTotal;
+		// return (superficie2 - superficie1);
+
+	}
+
+}
